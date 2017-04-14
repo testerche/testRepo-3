8f67374e1c1f44180781374be4fc5df0a3b7d1b4
idexmai <userefpk@some.mail>
2017-04-14 09:53:23 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1492159881021
+//first_change1492162448561
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
