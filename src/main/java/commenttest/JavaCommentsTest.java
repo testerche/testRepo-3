9d18318724d515a16235711d653e6cca618bc0c8
testerche <user9mhr@some.mail>
2017-04-19 16:00:48 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1492597259562
+//first_change1492615378250
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
