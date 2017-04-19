b73854eb5448795e1794c686b5461be8884696f0
testerche <userfssd@some.mail>
2017-04-19 11:01:23 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1492528330076
+//first_change1492597259562
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
